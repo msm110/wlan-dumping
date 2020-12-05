1c35fb7ad65dc8efa4fca52bebae26d284bab50e
msm <53619529+msm110@users.noreply.github.com>
2020-12-05 11:49:00 +0330

    Update wlan.sh

--- wlan.sh
+++ wlan.sh
@@ -10,10 +10,11 @@ else
 ifconfig wlan1 down
 ifconfig wlan0 down
 iwconfig $1 mode monitor
+service NetworkManager stop
 ifconfig $1 up
 iwconfig
 
-service NetworkManager stop
+
 
 airodump-ng $1
 
